1e357f440f3ebe8eb7b4ff6127a47d19fda79ee5
Yevhen Vorobiov <yevhen.vorobiov@nure.ua>
2018-12-10 00:36:41 +0200

    Followers and friends activity added

--- app/src/main/java/teteruk_vorobiov/edu/com/twitterclient/services/FollowersService.java
+++ app/src/main/java/teteruk_vorobiov/edu/com/twitterclient/services/FollowersService.java
@@ -7,5 +7,8 @@ import retrofit2.http.Query;
 
 public interface FollowersService {
     @GET("/1.1/followers/list.json")
-    Call<ResponseBody> list(@Query("user_id") long id);
+    Call<ResponseBody> listFollowers(@Query("user_id") long id);
+
+    @GET("/1.1/friends/list.json")
+    Call<ResponseBody> listFollowings(@Query("user_id") long id);
 }
